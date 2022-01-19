b825801eff4d5db2cd71308f413485b2c772824b
Manolingam N <manolingam1996@gmail.com>
2022-01-19 12:25:06 +0530

    text copy & card style update

--- components/splash/Membership.jsx
+++ components/splash/Membership.jsx
@@ -1,4 +1,13 @@
-import { Flex, SimpleGrid, Text } from '@chakra-ui/react';
+import {
+  Flex,
+  SimpleGrid,
+  Text,
+  List,
+  ListItem,
+  ListIcon
+} from '@chakra-ui/react';
+
+import { CheckIcon } from '@chakra-ui/icons';
 
 import styled from '@emotion/styled';
 import { theme } from '../../styles/theme';
@@ -19,14 +28,12 @@ const StyledHeading = styled(Text)`
 `;
 const StyledCard = styled(Flex)`
   flex-direction: column;
-  min-height: 380px;
   background: linear-gradient(180deg, #2b2c34 0%, #0f0f0e 100%);
   border-radius: 8px;
   padding: 20px 24px;
 `;
 const StyledCardHeading = styled(Text)`
   width: 100%;
-  min-height: 50px;
   font-family: ${theme.fonts.uncial};
   letter-spacing: 1.2px;
   color: ${theme.colors.red};
@@ -42,10 +49,12 @@ const StyledCostText = styled(Text)`
 `;
 const StyledCardText = styled(Text)`
   width: 100%;
-  font-family: ${theme.fonts.texturina};
+  text-transform: uppercase;
+  font-weight: bold;
+  font-family: ${theme.fonts.spaceMono};
   letter-spacing: 1.2px;
-  color: #fcfcfc;
-  text-align: left;
+  color: #7c3aed;
+  text-align: center;
   margin-bottom: 1rem;
 `;
 
@@ -53,19 +62,35 @@ const membership_levels = [
   {
     title: 'Never let it end',
     loot: '2400 loot in RaidBrood DAO',
-    desc: 'Premium DAO Membership VIP Access to All 2022 Events Case of Blood of Moloch Whitelisted for Limited Edition Mainnet NFT of brood recipes All base membership perks',
+    points: [
+      'VIP Access to All 2022 Events',
+      'Case of Blood of Moloch',
+      'Whitelisted for Limited Edition Mainnet NFT of brood recipes',
+      'All base membership perks'
+    ],
     cost: '666 xDai'
   },
   {
     title: 'One in each hand',
     loot: '200 loot in RaidBrood DAO',
-    desc: 'Special EthDenver Event Access EthDenver Tasting Room Access 2 Special Blood of Moloch Collector cans All base membership perks',
+    points: [
+      'Special EthDenver Event Access',
+      'EthDenver Tasting Room Access',
+      '2 Special Blood of Moloch Collector cans',
+      'All base membership perks'
+    ],
     cost: '66 xDai'
   },
   {
     title: 'Yeah you can come too',
     loot: '100 loot in RaidBrood DAO',
-    desc: ' Base Membership in RaidBrood DAO Governance on new beers and events Gift bag pick up at our events Limited edition Blood of Moloch collectors can Proof of yeet NFT',
+    points: [
+      'Base Membership in RaidBrood DAO',
+      'Governance on new beers and events',
+      'Gift bag pick up at our events',
+      'Limited edition Blood of Moloch collectors can',
+      'Proof of yeet NFT'
+    ],
     cost: '33 xDai'
   }
 ];
@@ -78,16 +103,30 @@ export const Membership = () => {
       </StyledHeading>
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap={10}>
         {membership_levels.map((level, index) => (
-          <StyledCard key={index}>
-            <StyledCardHeading fontSize={{ lg: '20px', base: '16px' }}>
+          <StyledCard minH={{ lg: '450px', sm: '400px' }} key={index}>
+            <StyledCardHeading
+              minH={{ lg: '70px', sm: '40px' }}
+              fontSize={{ lg: '20px', base: '16px' }}
+            >
               {level.title}
             </StyledCardHeading>
             <StyledCardText fontWeight='bold' my='1rem'>
               {level.loot}
             </StyledCardText>
-            <StyledCardText fontSize={{ lg: '14px', base: '12px' }}>
-              {level.desc}
-            </StyledCardText>
+            <List color='white' fontFamily={theme.fonts.spaceMono}>
+              {level.points.map((point, index) => (
+                <ListItem>
+                  <ListIcon
+                    as={CheckIcon}
+                    key={index}
+                    fontSize={{ lg: '14px', base: '12px' }}
+                  />
+
+                  {point}
+                </ListItem>
+              ))}
+            </List>
+            <br />
             <StyledCostText>{level.cost}</StyledCostText>
           </StyledCard>
         ))}
