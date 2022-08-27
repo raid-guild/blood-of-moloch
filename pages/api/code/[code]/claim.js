682c1f8c247bd265bcc26ab85f9c682a6315b4b4
Marcelo Paez Sequeira <paezao@gmail.com>
2022-08-27 07:35:29 -0300

    Check if code is claimed and claiming APIs

---
+++ pages/api/code/[code]/claim.js
@@ -0,0 +1,25 @@
+import CodeRepository from "../../../../lib/code_repository";
+import Contract from "../../../../lib/contract";
+
+const repository = new CodeRepository();
+const contract = new Contract();
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const { code } = req.query;
+        const { address } = req.body;
+
+        const foundCode = await repository.isValid(code);
+        if (!foundCode) {
+            return res.status(400).json({ error: "Code has already been claimed." });
+        }
+
+        try {
+            const tx = await contract.claim(address, code);
+            const receipt = await tx.wait();
+            return res.status(200).json(receipt);
+        } catch (err) {
+            return res.status(400).json({ error: err.message });
+        }
+    }
+}
---
+++ pages/api/code/[code]/is_claimed.js
@@ -0,0 +1,27 @@
+import CodeRepository from "../../../../lib/code_repository";
+import Contract from "../../../../lib/contract";
+
+const repository = new CodeRepository();
+const contract = new Contract();
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        const returnClaimed = (claimed) => {
+            return res.status(200).json({ claimed });
+        };
+
+        const { code } = req.query;
+
+        const foundCode = await repository.isValid(code);
+        if (!foundCode) {
+            return returnClaimed(true);
+        }
+
+        const claimed = await contract.isClaimed(code);
+        if (claimed) {
+            return returnClaimed(true);
+        }
+
+        return returnClaimed(false);
+    }
+}
