2376c790d5aa02f31353c61fa557215e6dcc0805
Han Valen <han@0xhanvalen.com>
2022-09-05 11:55:59 -0700

    more additions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-module.exports = {
-  reactStrictMode: true,
-}
-=======
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,4 +5,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
->>>>>>> 32fc2b8b7609d936d8b98ea2ddb5cbba2803bcaa
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-=======
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -13,7 +7,6 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
     </ChakraProvider>
   );
->>>>>>> 32fc2b8b7609d936d8b98ea2ddb5cbba2803bcaa
 }
 
 export default MyApp;
