7527c2d9fcd5921a4df81aad98c28b995c904d4c
Han Valen <han@0xhanvalen.com>
2022-09-09 14:41:03 -0700

    added ens lookup + new contract

--- pages/api/code/[code]/claim.js
+++ pages/api/code/[code]/claim.js
@@ -6,31 +6,39 @@ const repository = new CodeRepository();
 const contract = new Contract();
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-
-        const { code } = req.query;
-        const { address, password } = JSON.parse(req.body);
-        if (!password || password !== process.env.PASSWORD) {
-            return res.status(400).json({ error: "Code cannot be confirmed." });
-        }
-
-        const foundCode = await repository.isValid(code);
-        if (!foundCode) {
-            return res.status(400).json({ error: "Code has already been claimed." });
-        }
-
-        await contract.init();
-        
-        try {
-
-            const receipt = await contract.claim(address, code);
-            console.log("tx.status", receipt.status)
-            return res.status(200).json(receipt);
-        } catch (err) {
-            console.error(err);
-            return res.status(400).json({ error: err.message });
-        }
+  if (req.method === "POST") {
+    const { code } = req.query;
+    let { address, password } = JSON.parse(req.body);
+    if (!password || password !== process.env.PASSWORD) {
+      return res.status(400).json({ error: "Code cannot be confirmed." });
     }
 
-    return res.status(404);
+    const foundCode = await repository.isValid(code);
+    if (!foundCode) {
+      return res.status(400).json({ error: "Code has already been claimed." });
+    }
+
+    await contract.init();
+    const provider = ethers.providers.getDefaultProvider("homestead", {
+      alchemy:
+        "https://eth-mainnet.g.alchemy.com/v2/O7HNhorzCs5RCiGYizhOr1U8Uzy7xjMi",
+    });
+    let newAddress;
+    if (address.substring(address.length - 4, address.length) == ".eth") {
+        newAddress = await provider.resolveName(address);
+    } else {
+        newAddress = address;
+    }
+
+    try {
+      const receipt = await contract.claim(newAddress, code);
+      console.log("tx.status", receipt.status);
+      return res.status(200).json(receipt);
+    } catch (err) {
+      console.error(err);
+      return res.status(400).json({ error: err.message });
+    }
+  }
+
+  return res.status(404);
 }
