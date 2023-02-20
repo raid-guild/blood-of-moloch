c0caaaa4b740254569e3a1b76c50bea14a1a10e4
bitbeckers <code@bitbeckers.com>
2023-02-20 11:51:05 +0100

    feat(layout): cleanup beerinfo component

--- components/drink/BeerInfo.tsx
+++ components/drink/BeerInfo.tsx
@@ -9,56 +9,68 @@ import {
 } from "@chakra-ui/react";
 import { theme } from "../../styles/theme";
 
+type DescriptionProps = {
+  name: string;
+  content: string;
+};
+
+type TraitsProps = {
+  ibu: number;
+  abv: number;
+};
+const Description = ({ name, content }: DescriptionProps) => {
+  return (
+    <HStack w={"60%"} ml={"3em"}>
+      <Box style={{ fontFamily: `'futura-pt', sans-serif` }}>
+        <Heading fontFamily={theme.fonts.uncial}>{name.toUpperCase()}</Heading>
+        <Text w={"70%"} textAlign={"left"}>
+          {content}
+        </Text>
+      </Box>
+    </HStack>
+  );
+};
+
+const Traits = ({ ibu, abv }: TraitsProps) => {
+  const style = {
+    heading: {
+      fontFamily: `'futura-pt', sans-serif`,
+    },
+    divider: {
+      display: `block`,
+      height: `100px`,
+      width: `2px`,
+      backgroundColor: `#EBEBEB`,
+    },
+    data: {
+      fontSize: `40px`,
+      fontFamily: `'futura-pt', sans-serif`,
+      fontWeight: `200`,
+      color: `#898989`,
+    },
+  };
+
+  return (
+    <HStack>
+      <VStack>
+        <Heading sx={style.heading}>ABV</Heading>
+        <Text sx={style.data}>{`${abv} %`}</Text>
+      </VStack>
+      <div style={style.divider}></div>
+      <div>
+        <Heading sx={style.heading}>IBU</Heading>
+        <Text sx={style.data}>{ibu}</Text>
+      </div>
+    </HStack>
+  );
+};
+
 const BeerInfo = ({ bgColor, copy }) => {
   return (
     <Flex background={bgColor} py={"5em"} w={"100%"}>
-      <HStack w={"60%"} ml={"3em"}>
-        <Box style={{ fontFamily: `'futura-pt', sans-serif` }}>
-          <Heading fontFamily={theme.fonts.uncial}>
-            {copy.name.toUpperCase()}
-          </Heading>
-          <Text w={"70%"} textAlign={"left"}>
-            {copy.beer}
-          </Text>
-        </Box>
-      </HStack>
+      <Description name={copy.name} content={copy.beer} />
       <Divider orientation="vertical" />
-      <HStack>
-        <VStack>
-          <Heading sx={{ fontFamily: `'futura-pt', sans-serif` }}>ABV</Heading>
-          <Text
-            sx={{
-              fontSize: `40px`,
-              fontFamily: `'futura-pt', sans-serif`,
-              fontWeight: `200`,
-              color: `#898989`,
-            }}
-          >
-            {`${copy.traits.abv} %`}
-          </Text>
-        </VStack>
-        <div
-          style={{
-            display: `block`,
-            height: `100px`,
-            width: `2px`,
-            backgroundColor: `#EBEBEB`,
-          }}
-        ></div>
-        <div>
-          <Heading sx={{ fontFamily: `'futura-pt', sans-serif` }}>IBU</Heading>
-          <Text
-            sx={{
-              fontSize: `40px`,
-              fontFamily: `'futura-pt', sans-serif`,
-              fontWeight: `200`,
-              color: `#898989`,
-            }}
-          >
-            {copy.traits.ibu}
-          </Text>
-        </div>
-      </HStack>
+      <Traits ibu={copy.traits.ibu} abv={copy.traits.abv} />
     </Flex>
   );
 };
