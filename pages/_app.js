2c46fd87eb030ef9ecd2ea5916e16db53b50636d
bitbeckers <code@bitbeckers.com>
2023-02-21 18:49:19 -0700

    fix(code): cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Toaster } from "react-hot-toast";
-import customTheme from "../styles/theme";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={customTheme}>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
       <Toaster position="bottom-center" reverseOrder={false} />
     </ChakraProvider>
