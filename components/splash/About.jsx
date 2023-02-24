b5a295f89a2144d1da2ab83b8c09e089be1deaa2
bitbeckers <code@bitbeckers.com>
2023-02-24 09:19:09 -0700

    chore(merge): pffff

--- components/splash/About.jsx
+++ components/splash/About.jsx
@@ -1,7 +1,7 @@
-import { Flex, SimpleGrid, Text, Image } from "@chakra-ui/react";
-import styled from "@emotion/styled";
-import { theme } from "../../styles/theme";
-import { MEDIA_FILES } from "../../utils/constants";
+import { Flex, SimpleGrid, Text, Image } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { theme } from '../../styles/theme';
+import { MEDIA_FILES } from '../../utils/constants';
 
 const StyledContainer = styled(SimpleGrid)`
   grid-gap: 1rem;
@@ -17,7 +17,7 @@ const StyledSubContainer = styled(Flex)`
 const StyledHeading = styled(Text)`
   width: 100%;
   font-family: ${theme.fonts.uncial};
-  line-height: 72px;
+  line-height: 48px;
   color: white;
   text-align: left;
   margin-bottom: 1rem;
@@ -29,35 +29,49 @@ const StyledBodyText = styled(Text)`
   text-align: left;
 `;
 
+const StyledLink = styled.a`
+  color: #ff3864;
+  opacity: 1;
+  :hover {
+    opacity: 0.85;
+  }
+`;
+
 export const About = () => {
   return (
     <StyledContainer
       columns={{ base: 1, md: 2, lg: 2 }}
-      px={{ lg: "8rem", md: "4rem", base: "2rem" }}
+      px={{ lg: '8rem', md: '4rem', base: '2rem' }}
     >
       <StyledSubContainer>
-        <StyledHeading fontSize={{ xl: "54px", lg: "44px", base: "20px" }}>
+        <StyledHeading fontSize={{ xl: '54px', lg: '44px', base: '36px' }}>
           WTF is Raid Brood?!?
         </StyledHeading>
-        <StyledBodyText fontSize={{ lg: "18px", base: "12px" }}>
-          Raid Brood is the first of its kind, a DAO dedicated to craft beer.
+        <StyledBodyText fontSize={{ lg: '24px', base: '20px' }}>
+          Raid Brood is the first of its kind, a DAO dedicated to craft beer and
+          crypto.
         </StyledBodyText>
         <br />
-        <StyledBodyText fontSize={{ lg: "18px", base: "12px" }}>
-          Spun out from RaidGuild, our master brewer cooked up Blood of Moloch,
-          a special Russian Imperial Stout to commemorate EthDenver 2022. In the
-          spirit of Web3, we started a beer club as a DAO and invite you to
-          join, taste, and help shape what the next brew will be at the next
-          major ETH gathering.
+        <StyledBodyText fontSize={{ lg: '24px', base: '20px' }}>
+          Spun out from{' '}
+          <StyledLink href='https://www.raidguild.org/'>RaidGuild</StyledLink>,
+          our master brewer cooked up Blood of Moloch, a special Russian
+          Imperial Stout to commemorate EthDenver 2022. In the spirit of Web3,
+          we started a beer club as a{' '}
+          <StyledLink href='https://app.daohaus.club/dao/0x64/0xc6dd517a5d0e6c6962a8357ad47455c0f7b693bf'>
+            DAO
+          </StyledLink>{' '}
+          and invite you to join, taste, and help shape what the next brew will
+          be at the next major ETH gathering.
         </StyledBodyText>
       </StyledSubContainer>
       <Image
         src={MEDIA_FILES.illustrations.one}
-        alt="illustration"
-        w={{ lg: "450px", base: "200px" }}
-        ml="auto"
-        mr={{ lg: "0", base: "auto" }}
-        mt={{ base: "2rem", lg: "0", md: "0" }}
+        alt='illustration'
+        w={{ lg: '450px', base: '200px' }}
+        ml='auto'
+        mr={{ lg: '0', base: 'auto' }}
+        mt={{ base: '2rem', lg: '0', md: '0' }}
       />
     </StyledContainer>
   );
--- components/splash/Beers.jsx
+++ components/splash/Beers.jsx
@@ -1,8 +1,8 @@
-import { Flex, SimpleGrid, Text, Image, Box, Link } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Text, Image, Box, Link } from '@chakra-ui/react';
 
-import styled from "@emotion/styled";
-import { theme } from "../../styles/theme";
-import { BEER_CONTENT } from "../../utils/constants";
+import styled from '@emotion/styled';
+import { theme } from '../../styles/theme';
+import { BEER_CONTENT } from '../../utils/constants';
 
 const StyledContainer = styled(Flex)`
   flex-direction: column;
@@ -10,6 +10,7 @@ const StyledContainer = styled(Flex)`
   padding-top: 5rem;
   padding-bottom: 5rem;
 `;
+
 const StyledHeading = styled(Text)`
   width: 100%;
   font-family: ${theme.fonts.uncial};
@@ -18,13 +19,19 @@ const StyledHeading = styled(Text)`
   text-align: center;
   margin-bottom: 1rem;
 `;
+
 const StyledCard = styled(Flex)`
   width: 100%;
   position: absolute;
   flex-direction: column;
   border-radius: 8px;
   padding: 20px 24px;
+
+  @media (max-width: 480px) {
+    padding: 0 12px;
+  }
 `;
+
 const StyledCardHeading = styled(Text)`
   width: 100%;
   font-family: ${theme.fonts.uncial};
@@ -32,10 +39,11 @@ const StyledCardHeading = styled(Text)`
   color: ${theme.colors.red};
   text-align: center;
 `;
+
 const StyledCardText = styled(Text)`
   width: 100%;
   font-family: ${theme.fonts.sourceSansPro};
-  color: ${(color) => color};
+  color: ${color => color};
   text-align: left;
   margin-top: 1rem;
 `;
@@ -55,45 +63,61 @@ const StyledBox = styled(Box)`
   }
 `;
 
+const Grid = styled.div`
+  padding-top: 72px;
+
+  @media (max-width: 480px) {
+    padding-top: 12px;
+  }
+`;
+
+const Label = styled.div`
+  padding-top: 12px;
+`;
+
 export const Beers = () => {
   return (
-    <StyledContainer px={{ lg: "8rem", md: "4rem", base: "2rem" }}>
-      <StyledHeading fontSize={{ lg: "42px", base: "30px" }}>
+    <StyledContainer px={{ lg: '8rem', md: '4rem', base: '2rem' }}>
+      <StyledHeading fontSize={{ xl: '54px', lg: '44px', base: '36px' }}>
         Our Brood
       </StyledHeading>
-      <StyledBodyText fontSize={{ lg: "18px", base: "12px" }}>
-        At RaidBrood, brews are our weapons in the fight against Moloch. Armed
+      <StyledBodyText fontSize={{ lg: '24px', base: '20px' }}>
+        At Raid Brood, brews are our weapons in the fight against Moloch. Armed
         with grains, hops, yeast and water, our master brewers are practiced in
         the arts and sciences of brewish battle. This is our arsenal.
       </StyledBodyText>
-      <SimpleGrid columns={{ base: 1, md: 1, lg: 2, xl: 2 }} gap={10}>
-        {BEER_CONTENT.map((beer, index) => (
-          <StyledBox
-            key={index}
-            minH={{ xl: "585px", lg: "525px", md: "425px", sm: "450px" }}
-            position="relative"
-            cursor="pointer"
-          >
-            <Link href={beer.path}>
-              <StyledCard h="100%" key={index}>
-                <StyledCardHeading
-                  minH={{ lg: "75px", sm: "40px" }}
-                  fontSize={{ lg: "36px", base: "16px" }}
-                >
-                  {beer.title}
-                </StyledCardHeading>
-                <Image src={beer.label} alt="cans" maxH="100%" />
-                <StyledCardText
-                  color="#B66AD6"
-                  fontSize={{ lg: "16px", base: "16px" }}
-                >
-                  {beer.description}
-                </StyledCardText>
-              </StyledCard>
-            </Link>
-          </StyledBox>
-        ))}
-      </SimpleGrid>
+      <Grid>
+        <SimpleGrid columns={{ base: 1, md: 1, lg: 2, xl: 2 }} gap={10}>
+          {BEER_CONTENT.map((beer, index) => (
+            <StyledBox
+              key={index}
+              minH={{ xl: '625px', lg: '575px', md: '500px', sm: '500px' }}
+              position='relative'
+              cursor='pointer'
+            >
+              <Link href={beer.path}>
+                <StyledCard h='100%' key={index}>
+                  <StyledCardHeading
+                    minH={{ lg: '75px', sm: '40px' }}
+                    fontSize={{ lg: '48px', base: '24px' }}
+                  >
+                    {beer.title}
+                  </StyledCardHeading>
+                  <Label>
+                    <Image src={beer.label} alt='cans' maxH='100%' />
+                  </Label>
+                  <StyledCardText
+                    color='#B66AD6'
+                    fontSize={{ lg: '20px', base: '16px' }}
+                  >
+                    {beer.description}
+                  </StyledCardText>
+                </StyledCard>
+              </Link>
+            </StyledBox>
+          ))}
+        </SimpleGrid>
+      </Grid>
     </StyledContainer>
   );
 };
