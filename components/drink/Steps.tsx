fb873108472652153ecf0ec17eb1b2360c12e99e
earth2travis <travis.mccutcheon@gmail.com>
2023-02-26 11:57:36 -0600

    Update content on brews pages

--- components/drink/Steps.tsx
+++ components/drink/Steps.tsx
@@ -1,5 +1,5 @@
-import { VStack, Heading, OrderedList, ListItem } from "@chakra-ui/react";
-import { theme } from "../../styles/theme";
+import { VStack, Heading, OrderedList, ListItem } from '@chakra-ui/react';
+import { theme } from '../../styles/theme';
 
 type StepsProps = {
   callToAction: string;
@@ -7,19 +7,19 @@ type StepsProps = {
 };
 const Steps = ({ callToAction, bgColor }: StepsProps) => {
   return (
-    <VStack backgroundColor={bgColor} w={"100%"} py={"5em"}>
-      <Heading fontFamily={theme.fonts.uncial} textAlign={"left"}>
+    <VStack backgroundColor={bgColor} w={'100%'} py={'5em'}>
+      <Heading fontFamily={theme.fonts.uncial} textAlign={'left'}>
         Start Here
       </Heading>
       <OrderedList fontFamily={theme.fonts.sourceSansPro}>
         <ListItem>{callToAction}</ListItem>
-        <ListItem>Scan your QR code &amp; Enter the Secret Password</ListItem>
+        <ListItem>Scan your QR code</ListItem>
         <ListItem>
           <span style={{ fontWeight: `700`, color: `#E0232C` }}>
-            Claim the Raid Brood NFT
-          </span>{" "}
-          &amp; become a DAOist
+            Claim the RaidBrood NFT
+          </span>
         </ListItem>
+        <ListItem>Get DAO Shares</ListItem>
       </OrderedList>
     </VStack>
   );
