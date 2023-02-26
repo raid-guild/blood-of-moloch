ad8028c400d3e2a90c865a2b7b6f8a7c45cbe4b4
earth2travis <travis.mccutcheon@gmail.com>
2023-02-26 12:08:10 -0600

    Update discord links

--- components/drink/LogoHeader.tsx
+++ components/drink/LogoHeader.tsx
@@ -1,37 +1,37 @@
-import { Image, Center, VStack, HStack, Spacer } from "@chakra-ui/react";
-import { MEDIA_FILES } from "../../utils/constants";
+import { Image, Center, VStack, HStack, Spacer } from '@chakra-ui/react';
+import { MEDIA_FILES } from '../../utils/constants';
 
 const LogoHeader = ({ path }) => {
   return (
-    <VStack w={"100%"} py={"3em"} bgGradient="linear(to-b, #2b2c34, black)">
-      <HStack w={"80%"}>
-        <Image src={MEDIA_FILES.logos.footer} alt="logo" boxSize={"4xs"} />
+    <VStack w={'100%'} py={'3em'} bgGradient='linear(to-b, #2b2c34, black)'>
+      <HStack w={'80%'}>
+        <Image src={MEDIA_FILES.logos.footer} alt='logo' boxSize={'4xs'} />
         <Spacer />
-        <HStack justifyContent={"flex-end"}>
+        <HStack justifyContent={'flex-end'}>
           <Image
             src={MEDIA_FILES.icons.discord}
-            alt="discord"
-            w={{ lg: "32px", base: "18px" }}
-            mr=".5rem"
-            cursor="pointer"
+            alt='discord'
+            w={{ lg: '32px', base: '18px' }}
+            mr='.5rem'
+            cursor='pointer'
             onClick={() =>
-              window.open("https://discord.gg/XKGM8u8XTQ", "_blank")
+              window.open('https://discord.gg/AMHbFZdcHs', '_blank')
             }
           />
           <Image
             src={MEDIA_FILES.icons.twitter}
-            alt="twitter"
-            w={{ lg: "32px", base: "18px" }}
-            cursor="pointer"
+            alt='twitter'
+            w={{ lg: '32px', base: '18px' }}
+            cursor='pointer'
             onClick={() =>
-              window.open("https://twitter.com/RaidBrood", "_blank")
+              window.open('https://twitter.com/RaidBrood', '_blank')
             }
           />
         </HStack>
       </HStack>
 
       <Center>
-        <Image src={path} objectFit="contain" boxSize={"md"} alt="drink logo" />
+        <Image src={path} objectFit='contain' boxSize={'md'} alt='drink logo' />
       </Center>
     </VStack>
   );
--- shared/Header.jsx
+++ shared/Header.jsx
@@ -1,6 +1,6 @@
-import { Flex, Image } from "@chakra-ui/react";
-import styled from "@emotion/styled";
-import { MEDIA_FILES } from "../utils/constants";
+import { Flex, Image } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { MEDIA_FILES } from '../utils/constants';
 
 const StyledContainer = styled(Flex)`
   width: 100%;
@@ -12,35 +12,35 @@ const StyledContainer = styled(Flex)`
 
 export const Header = () => {
   return (
-    <StyledContainer px={{ lg: "8rem", md: "4rem", base: "2rem" }}>
+    <StyledContainer px={{ lg: '8rem', md: '4rem', base: '2rem' }}>
       <Image
-        position="absolute"
+        position='absolute'
         left={0}
         right={0}
-        ml="auto"
-        mr="auto"
+        ml='auto'
+        mr='auto'
         src={MEDIA_FILES.logos.header}
-        alt="logo"
-        cursor="pointer"
-        onClick={() => (window.location.href = "/")}
-        w={{ lg: "170px", base: "100px" }}
+        alt='logo'
+        cursor='pointer'
+        onClick={() => (window.location.href = '/')}
+        w={{ lg: '170px', base: '100px' }}
       />
 
-      <Flex direction="row" alignItems="center">
+      <Flex direction='row' alignItems='center'>
         <Image
           src={MEDIA_FILES.icons.discord}
-          alt="discord"
-          w={{ lg: "32px", base: "18px" }}
-          mr=".5rem"
-          cursor="pointer"
-          onClick={() => window.open("https://discord.gg/XKGM8u8XTQ", "_blank")}
+          alt='discord'
+          w={{ lg: '32px', base: '18px' }}
+          mr='.5rem'
+          cursor='pointer'
+          onClick={() => window.open('https://discord.gg/AMHbFZdcHs', '_blank')}
         />
         <Image
           src={MEDIA_FILES.icons.twitter}
-          alt="twitter"
-          w={{ lg: "32px", base: "18px" }}
-          cursor="pointer"
-          onClick={() => window.open("https://twitter.com/RaidBrood", "_blank")}
+          alt='twitter'
+          w={{ lg: '32px', base: '18px' }}
+          cursor='pointer'
+          onClick={() => window.open('https://twitter.com/RaidBrood', '_blank')}
         />
       </Flex>
     </StyledContainer>
