c7dfd93c5f26f06113e21078cc9f3cd7dd6a2455
earth2travis <travis.mccutcheon@gmail.com>
2023-02-26 11:09:38 -0600

    Update homepage content

--- components/splash/Brew.jsx
+++ components/splash/Brew.jsx
@@ -1,7 +1,7 @@
-import { Flex, Image, Link, Text } from "@chakra-ui/react";
-import { PopupButton } from "@typeform/embed-react";
-import styled from "@emotion/styled";
-import { theme } from "../../styles/theme";
+import { Flex, Image, Link, Text } from '@chakra-ui/react';
+import { PopupButton } from '@typeform/embed-react';
+import styled from '@emotion/styled';
+import { theme } from '../../styles/theme';
 
 const StyledContainer = styled(Flex)`
   flex-direction: column;
@@ -49,29 +49,40 @@ const StyledLink = styled.a`
 
 export const Brew = () => {
   return (
-    <StyledContainer px={{ lg: "8rem", md: "4rem", base: "2rem" }}>
-      <Flex w="100%" direction="column" justifyContent="space-between">
-        <StyledHeading fontSize={{ xl: "54px", lg: "44px", base: "36px" }}>
+    <StyledContainer px={{ lg: '8rem', md: '4rem', base: '2rem' }}>
+      <Flex w='100%' direction='column' justifyContent='space-between'>
+        <StyledHeading fontSize={{ xl: '54px', lg: '44px', base: '36px' }}>
           Brew a Batch
         </StyledHeading>
-        <StyledBodyText fontSize={{ lg: "24px", base: "20px" }}>
-          Raid Brood brews custom batches for communities, conferences, and
-          other events. Example brew collaborators include{" "}
-          <StyledLink href="https://greenpill.party/">
+        <StyledBodyText fontSize={{ lg: '24px', base: '20px' }}>
+          RaidBrood brews custom batches for communities, conferences, and other
+          events.
+        </StyledBodyText>
+        <StyledBodyText fontSize={{ lg: '24px', base: '20px' }}>
+          Our team of beer experts work to concoct the perfect brew for you. Our
+          master brewers capture the tastes and notes that encapsulate what you
+          want to do with your brew. Our designers create a logo that
+          communicates your brew&apos;s essence. Our developers create an NFT
+          for your brew. Our writers pick the perfect words to describe the lore
+          of your brew.
+        </StyledBodyText>
+        <StyledBodyText fontSize={{ lg: '24px', base: '20px' }}>
+          Collaborators include{' '}
+          <StyledLink href='https://greenpill.party/'>
             GreenPill Podcast
-          </StyledLink>{" "}
-          and{" "}
-          <StyledLink href="https://www.sporkdao.org/">
+          </StyledLink>{' '}
+          and{' '}
+          <StyledLink href='https://www.sporkdao.org/'>
             SporkDAO/ETHDenver 2023
           </StyledLink>
           .
         </StyledBodyText>
-        <StyledBodyText fontSize={{ lg: "24px", base: "20px" }}>
+        <StyledBodyText fontSize={{ lg: '24px', base: '20px' }}>
           Do you want to join us in the endeavour of exlixir to fuel our fight
           against Moloch by making a batch for you?
         </StyledBodyText>
       </Flex>
-      <StyledPopupButton id="KSaCPC3g" className="my-button">
+      <StyledPopupButton id='KSaCPC3g' className='my-button'>
         Contact Us
       </StyledPopupButton>
     </StyledContainer>
