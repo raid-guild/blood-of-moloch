986f4afbe8e63a5f6c87dc0eeead624d88d67e30
bitbeckers <code@bitbeckers.com>
2023-02-26 18:04:26 -0700

    feat(link): restored DAO link

--- components/drink/Steps.tsx
+++ components/drink/Steps.tsx
@@ -9,19 +9,22 @@ type StepsProps = {
 };
 const Steps = ({ callToAction, bgColor, daoClaimUrl }: StepsProps) => {
   return (
-    <VStack backgroundColor={bgColor} w={'100%'} py={'5em'}>
-      <Heading fontFamily={theme.fonts.uncial} textAlign={'left'}>
+    <VStack backgroundColor={bgColor} w={"100%"} py={"5em"}>
+      <Heading fontFamily={theme.fonts.uncial} textAlign={"left"}>
         Start Here
       </Heading>
       <OrderedList fontFamily={theme.fonts.sourceSansPro}>
         <ListItem>{callToAction}</ListItem>
         <ListItem>Scan your QR code</ListItem>
+        <ListItem>Claim the RaidBrood NFT</ListItem>
         <ListItem>
-          <span style={{ fontWeight: `700`, color: `#E0232C` }}>
-            Claim the RaidBrood NFT
-          </span>
+          <Link href={daoClaimUrl}>
+            <a style={{ fontWeight: `700`, color: `#E0232C` }}>
+              Claim your DAO shares{" "}
+            </a>
+          </Link>
         </ListItem>
-        <ListItem>Get DAO Shares</ListItem>
+        ]
       </OrderedList>
     </VStack>
   );
