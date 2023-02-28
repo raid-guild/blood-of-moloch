1f33b5da8666d1512dc08e066d5cf1a6391d634d
skuhlmann <samkuhlmann@gmail.com>
2023-02-28 12:33:39 -0700

    ready to go

--- pages/[drink]/claim/index.tsx
+++ pages/[drink]/claim/index.tsx
@@ -1,12 +1,12 @@
+import { useRouter } from "next/router";
 import { Footer } from "../../../shared/Footer";
-import {
-  Box,
-  Heading,
-  VStack,
-  Center,
-} from "@chakra-ui/react";
+import { Box, Heading, VStack, Center } from "@chakra-ui/react";
+import Label from "../../../components/drink/Label";
 
 const Claim = () => {
+  const router = useRouter();
+  const { drink } = router.query;
+
   return (
     <Box h={"100vh"} bgColor={" #2b2c34"}>
       <VStack bgGradient="linear(to-b, black, #2b2c34)" spacing={"3em"}>
